b98fec67af4db10a8ff5c518521194bb1a604a73
NS997 <nstanisic997@gmail.com>
2022-11-16 19:21:25 +0100

    Initial commit

---
+++ src/main/java/ns/bootandangular/BootAndAngularApplication.java
@@ -0,0 +1,13 @@
+package ns.bootandangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAndAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootAndAngularApplication.class, args);
+    }
+
+}
